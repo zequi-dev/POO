c0a074c92fff28f3a37f47061bfe68566fde1690
zequi-dev <115948898+zequi-dev@users.noreply.github.com>
2024-05-30 20:23:14 -0700

    Add files via upload

---
+++ Practica LAB 2.3.1/Persona.java
@@ -0,0 +1,71 @@
+public class Persona {
+    protected String nombre; 
+    private int edad; 
+    private String genero;
+    private String direccion;
+    protected String profesion; 
+
+    public Persona(String nombre, int edad, String genero, String direccion, String profesion) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.genero = genero;
+        this.direccion = direccion;
+        this.profesion = profesion;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setProfesion(String profesion) {
+        this.profesion = profesion;
+    }
+
+    public String getProfesion() {
+        return profesion;
+    }
+
+    public void comer(String comida) {
+        System.out.println("La persona está comiendo " + comida);
+    }
+
+    protected void dormir(int horas) {
+        System.out.println("La persona está durmiendo durante " + horas + " horas");
+    }
+
+    private void tomarShower(int hora) {
+        System.out.println("La persona está tomando una ducha a las " + hora + " horas");
+    }
+
+    public void tomarDucha(int hora) {
+        tomarShower(hora);
+    }
+}
