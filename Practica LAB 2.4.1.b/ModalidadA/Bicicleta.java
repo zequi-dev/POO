c0815e0703b9d696b8d32807616421926e40f2c9
zequi-dev <115948898+zequi-dev@users.noreply.github.com>
2024-05-31 02:21:06 -0700

    Add files via upload

---
+++ Practica LAB 2.4.1.b/ModalidadA/Bicicleta.java
@@ -0,0 +1,58 @@
+// Bicicleta.java
+public class Bicicleta {
+    private String marca;
+    private String modelo;
+    private int ano;
+
+    public Bicicleta(String marca, String modelo, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    // Clase anidada Rueda
+    public class Rueda {
+        private double diametro;
+
+        public Rueda(double diametro) {
+            this.diametro = diametro;
+        }
+
+        public double getDiametro() {
+            return diametro;
+        }
+
+        public void setDiametro(double diametro) {
+            this.diametro = diametro;
+        }
+    }
+
+    // Clase anidada Cuadro
+    public class Cuadro {
+        private String material;
+
+        public Cuadro(String material) {
+            this.material = material;
+        }
+
+        public String getMaterial() {
+            return material;
+        }
+
+        public void setMaterial(String material) {
+            this.material = material;
+        }
+    }
+}
---
+++ Practica LAB 2.4.1.b/ModalidadB/ModalidadB.java
@@ -0,0 +1,18 @@
+
+public class ModalidadB {
+    public static void main(String[] args) {
+        Bicicleta bicicleta = new Bicicleta("Giant", "Defy", 2021);
+
+        Bicicleta.Rueda ruedaDelantera = bicicleta.new Rueda(26);
+        Bicicleta.Rueda ruedaTrasera = bicicleta.new Rueda(26);
+        Bicicleta.Cuadro cuadro = bicicleta.new Cuadro("Carbono");
+
+        // Mostrar información de la bicicleta y sus componentes
+        System.out.println("Marca: " + bicicleta.getMarca());
+        System.out.println("Modelo: " + bicicleta.getModelo());
+        System.out.println("Año: " + bicicleta.getAno());
+        System.out.println("Material del cuadro: " + cuadro.getMaterial());
+        System.out.println("Diámetro de la rueda delantera: " + ruedaDelantera.getDiametro());
+        System.out.println("Diámetro de la rueda trasera: " + ruedaTrasera.getDiametro());
+    }
+}
